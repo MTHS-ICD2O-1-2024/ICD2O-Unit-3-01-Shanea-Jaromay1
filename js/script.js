62d39b7edb3f7b82924ee3fa3c243f12b1dad075
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-03-28 16:45:20 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Shanea Jaromay All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Shanea Jaromay
+// Created on: March 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateAreaOfTrapezoid () {
+  // input
+  const aBaseOfTrapezoid = parseInt(document.getElementById('a-base-of-trapezoid').value)
+  const bBaseOfTrapezoid = parseFloat(document.getElementById('b-base-of-trapezoid').value)
+  const heightOfTrapezoid = parseInt(document.getElementById('height-of-trapezoid').value)
+
+  // process
+  const areaOfTrapezoid = [ (aBaseOfTrapezoid + bBaseOfTrapezoid) / 2] * heightOfTrapezoid
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTrapezoid + ' cm²'
+}
+
