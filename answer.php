d71c09b9ec8d99344e01e63efd5a28cab66868ee
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-03-28 17:02:52 +0000

    delete

---
+++ answer.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="The Hello World program, in PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Shanea Jaromay" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.yellow-indigo.min.css" />
+  <link rel=" apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
+  <link rel="manifest" href="./site.webmanifest" />
+  <title>Address,in PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Address, in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/street.jpg" alt="street image" width="250" />
+      </div>
+      <div class="page-content-php">
+        <div id="address">
+          <?php
+          $number = $_GET["street-number"];
+          $name = $_GET["street-name"];
+
+          echo "<p>Your address is: " . $number . ", name " . $name . ".</p>";
+          ?>
+        </div>
+      </div>
+    </main>
+  </div>
+</body>
+
+</html>
